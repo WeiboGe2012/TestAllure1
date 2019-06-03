9b8dabceff28390db6ebbf0361dcb347e9e50cc8
kutouqq <396700196@qq.com>
2019-06-03 23:02:41 +0800

    test

---
+++ scripts/taoge.py
@@ -0,0 +1,19 @@
+import pytest, allure
+
+
+class TestAbc:
+
+    @allure.step(title="测试步骤001")
+    @allure.severity(allure.severity_level.CRITICAL)
+    def test_abc(self):
+        allure.attach('描述', '我是测试步骤001的描述～～～')
+        assert 1
+
+    @allure.severity(allure.severity_level.BLOCKER)
+    def test_b(self):
+        allure.attach('描述', '我是测试步骤002的描述～～～')
+        assert 0
+
+
+if __name__ == '__main__':
+    pytest.main("-s --alluredir report taoge.py")
